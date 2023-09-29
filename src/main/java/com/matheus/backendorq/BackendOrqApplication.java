60d75ea773cf8cc09d325abd47f72f1e5e6def4c
Matheus <matheusrcdtms@hotmail.com>
2023-09-29 10:53:01 -0300

    init

---
+++ src/main/java/com/matheus/backendorq/BackendOrqApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.backendorq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOrqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOrqApplication.class, args);
+	}
+
+}
